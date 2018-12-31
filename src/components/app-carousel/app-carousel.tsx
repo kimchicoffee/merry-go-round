f9fafc98a69cf6887224cbd8cee65d4fe03f01d8
Progenitus <progenitus@Progenituss-MacBook-Pro.local>
2018-12-30 19:53:00 -0500

    Add images

--- src/components/app-carousel/app-carousel.tsx
+++ src/components/app-carousel/app-carousel.tsx
@@ -1,15 +1,26 @@
-import { Component } from '@stencil/core';
+import { Component, State } from "@stencil/core";
 
 @Component({
-  tag: 'app-carousel',
-  styleUrl: 'app-carousel.css',
+  tag: "app-carousel",
+  styleUrl: "app-carousel.css"
 })
 export class AppCarousel {
+  @State() images = [
+    "https://loremflickr.com/320/240/seoul",
+    "https://loremflickr.com/320/240/tokyo",
+    "https://loremflickr.com/320/240/paris"
+  ];
 
   render() {
     return (
-      <div>
-"this is carousel"
+      <div class="app-carousel">
+        <ul class="app-carousel-images">
+          {this.images.map(img => (
+            <li class="app-carousel-image">
+              <img src={img} />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
